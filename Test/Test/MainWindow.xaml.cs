8e9a240a34063a2611681ea2a2eaaba121b54d55
LeVietMinh <levietminh6677@gmail.com>
2024-11-16 22:27:28 +0700

    Initial Commit

---
+++ Test/Test/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private string connectionString = "Data Source=localhost;Initial Catalog=contact;Integrated Security=True";
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = Username.Text;
+            string password = Password.Password;
+
+            if(String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Không được để trống Username hoặc Password");
+                return;
+            }
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT COUNT(1) FROM Users WHERE Username=@username AND Password=@password";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                if (count == 1)
+                {
+                    MessageBox.Show("Đăng nhập thành công!");
+                }
+                else
+                {
+                    MessageBox.Show("Username hoặc password không đúng");
+                }
+            }
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 register = new Window1();
+            register.Show();
+            this.Hide();
+
+        }
+    }
+}
---
+++ Test/Test/Window1.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        private string connectionString = "Data Source=localhost;Initial Catalog=contact;Integrated Security=True";
+        public enum EnumRoles
+        {
+            GiamDoc,
+            NhanVien,
+            Khach
+        }
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            string username = Username.Text;
+            string password = Password.Password;
+            string firstname = FirstName.Text;
+            string lastname = LastName.Text;
+            string _roles = Roles.SelectedItem.ToString();
+            EnumRoles temproles = (EnumRoles)Enum.Parse(typeof(EnumRoles), _roles);
+            int roles = (int)temproles;
+            if(String.IsNullOrWhiteSpace(username) || String.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Không được để trống username hoặc password");
+                return;
+            }  
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "INSERT INTO Users (Username, Password, FirstName, Lastname, Roles) VALUES (@username, @password, @firstname, @lastname, @roles)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.Parameters.AddWithValue("@firstname", firstname);
+                cmd.Parameters.AddWithValue("@lastname", lastname);
+                cmd.Parameters.AddWithValue("@roles", roles);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Đăng ký thành công");
+
+                MainWindow login = new MainWindow();
+                login.Show();
+                this.Close();
+            }
+        }
+
+        private void Return_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow login = new MainWindow();
+            login.Show();
+            this.Close();
+        }
+
+        private void Roles_Loaded(object sender, RoutedEventArgs e)
+        {
+            Roles.Items.Add("GiamDoc");
+            Roles.Items.Add("NhanVien");
+            Roles.Items.Add("Khach");
+        }
+    }
+}
